91e2d055f14644daf89e64c570ce4d26e6bf6772
ssande-fwd <ssande@forwardedu.com>
2026-04-06 13:05:53 -0400

    enum

---
+++ common/enums.ts
@@ -0,0 +1,11 @@
+// If an enum could conceivably be reusable put it here.
+// Else keep it with it's associated module.
+
+namespace opensciedEnums {
+    export const enum OverUnder {
+        //% block="over"
+        Over,
+        //% block="under"
+        Under,
+    }
+}
